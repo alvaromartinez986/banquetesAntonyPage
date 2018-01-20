a2f9b43023dcd90969e1fea328a6259d1fa8ffbc
alvaromartinez986 <msae986@gmail.com>
2018-01-20 08:12:42 -0500

    Add a animation to navbar scroll and fix font style

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,19 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) private doc: Document) { }
+  public fixed: boolean = false;
+
+  constructor(public el: ElementRef) { }
 
   ngOnInit() {
   }
 
- @HostListener("window:scroll", [])
- onWindowScroll() {
-    let num = this.doc.body.scrollTop;
-    if ( num > 50 ) {
+  @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+      let componentPosition = this.el.nativeElement;
+      let scrollPosition = window.pageYOffset;
+      console.log(componentPosition);
+
+      if (scrollPosition >= 300) {
         this.fixed = true;
-    }else if (this.fixed && num < 5) {
-        this.fixed = false;
+      } else {
+        this.fixed = false
+      }
+
     }
- }
 
 }
