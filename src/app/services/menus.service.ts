17ab5a680a54c168084a2ae2f8b041a2b8ac3b8c
amartine <amartinez@pros.com>
2018-01-26 17:19:11 -0500

    Add menu content

--- src/app/services/menus.service.ts
+++ src/app/services/menus.service.ts
@@ -9,7 +9,50 @@ export class MenusService {
       type : "(1 Carne)",
       description: "",
       image: "assets/img/1Carne.jpg"
-    }
+    },
+    {
+      name : "Gourmet",
+      type : "(2 Carnes)",
+      description: "",
+      image: "assets/img/2Carnes.jpg"
+    },
+    {
+      name : "Arroces",
+      type : "",
+      description: "",
+      image: "assets/img/Arroces.jpeg"
+    },
+    {
+      name : "Desayunos",
+      type : "",
+      description: "",
+      image: "assets/img/1Carne.jpg"
+    },
+    {
+      name : "Refrigerios",
+      type : "",
+      description: "",
+      image: "assets/img/refrigerios.jpeg"
+    },
+    {
+      name : "Pasabocas",
+      type : "",
+      description: "",
+      image: "assets/img/pasabocas.jpg"
+    },
+    {
+      name : "Pastas",
+      type : "",
+      description: "",
+      image: "assets/img/pasta.jpg"
+    },
+    {
+      name : "Criolla",
+      type : "",
+      description: "",
+      image: "assets/img/criolla.jpeg"
+    },
+
   ]
 
   constructor() { }
