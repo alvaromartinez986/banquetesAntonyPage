b8f6b0b7a1ef2bf18b66b20ac56e43bf4d1e8c5f
alvaromartinez986 <msae986@gmail.com>
2018-02-11 17:43:56 -0500

    Complete footer section, modufy url clients and fix issues urls broken

--- src/app/services/clients.service.ts
+++ src/app/services/clients.service.ts
@@ -6,33 +6,33 @@ export class ClientsService {
   private clients:Client[] = [
     {
       name : "",
-      url : "",
-      image: "assets/img/companies/1.png"
+      url : "http://acecolombia.org/acecolombia/",
+      image: "assets/img/companies/acecolombia.png"
     },
     {
       name : "",
-      url : "",
-      image: "assets/img/companies/2.jpg"
+      url : "http://www.colgate.com.co/es/co/oc/",
+      image: "assets/img/companies/Colgate_Palmolive.gif"
     },
     {
       name : "",
-      url : "",
-      image: "assets/img/companies/3.gif"
+      url : "https://www.facebook.com/eventos.orioncali/",
+      image: "assets/img/companies/eventos_orion.gif"
     },
     {
       name : "",
-      url : "",
-      image: "assets/img/companies/4.jpg"
+      url : "http://tubosyperfilesfanalca.com/",
+      image: "assets/img/companies/fanalca.jpg"
     },
     {
       name : "",
-      url : "",
-      image: "assets/img/companies/5.png"
+      url : "http://www.calieventos.com/la-hacienda.html",
+      image: "assets/img/companies/la_hacienda.png"
     },
     {
       name : "",
-      url : "",
-      image: "assets/img/companies/6.png"
+      url : "http://novatecfs.com/",
+      image: "assets/img/companies/nfs.png"
     },
   ]
 
